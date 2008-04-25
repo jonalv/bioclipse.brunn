5623028f1f9136c311dda4b39940b3633ed7bdc7
jonalv <jonalv@1888e441-f645-0410-a580-a9c2aaa8c07a>
2008-04-25 13:22:58 +0000

    removed debug sysout

--- plugins/net.bioclipse.brunn.ui/src/net/bioclipse/brunn/ui/explorer/model/folders/AbstractFolder.java
+++ plugins/net.bioclipse.brunn.ui/src/net/bioclipse/brunn/ui/explorer/model/folders/AbstractFolder.java
@@ -82,7 +82,6 @@ public abstract class AbstractFolder extends Model
 			}
 			
 			public void visit(PatientOrigin patientOrigin) {
-				System.out.println("in AbstractFolder visit(PatientOrigin patientOrigin)");
 				finalChildren.add(
 						new PatientSample(abstractFolder, patientOrigin) );
 			}
