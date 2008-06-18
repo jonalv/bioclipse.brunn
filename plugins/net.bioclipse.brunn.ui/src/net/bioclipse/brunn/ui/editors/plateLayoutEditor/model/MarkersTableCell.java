35c302fbe53f766be7c364e2827948f2187cc2cd
jonalv <jonalv@1888e441-f645-0410-a580-a9c2aaa8c07a>
2008-06-18 16:05:31 +0000

    sorted markers in cells in platelayouteditor

--- plugins/net.bioclipse.brunn.ui/src/net/bioclipse/brunn/ui/editors/plateLayoutEditor/model/MarkersTableCell.java
+++ plugins/net.bioclipse.brunn.ui/src/net/bioclipse/brunn/ui/editors/plateLayoutEditor/model/MarkersTableCell.java
@@ -1,7 +1,11 @@
 package net.bioclipse.brunn.ui.editors.plateLayoutEditor.model;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+import com.sun.org.apache.xml.internal.utils.StringComparable;
 
 public class MarkersTableCell {
 
@@ -30,6 +34,17 @@ public class MarkersTableCell {
 	
 	public void addMarker(String name) {
 		markers.add(name);
+		Collections.sort(markers, new Comparator<String>() {
+			@Override
+			public int compare(String arg0, String arg1) {
+				if ( arg0.charAt(0) == 'M' &&
+					 arg0.charAt(0) == 'M'	) {
+					return Integer.parseInt( arg0.substring(1) ) - 
+					       Integer.parseInt( arg1.substring(1) );
+				}
+				return arg0.compareTo(arg1);
+			}
+		} );
 	}
 	
 	public int numberOfFunctions() {
