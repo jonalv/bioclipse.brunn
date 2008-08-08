2462312e93cf40f8eb06897e3c1f4b55aa571cd3
jonalv <jonalv@1888e441-f645-0410-a580-a9c2aaa8c07a>
2008-08-08 13:55:29 +0000

    Fixed problem with reading database settings from Usermanager. Now it does that.

--- plugins/net.bioclipse.brunn.core/src/net/bioclipse/brunn/Springcontact.java
+++ plugins/net.bioclipse.brunn.core/src/net/bioclipse/brunn/Springcontact.java
@@ -1,22 +1,14 @@
 package net.bioclipse.brunn;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.URL;
+import java.util.List;
 
-import net.bioclipse.usermanager.AccountType;
 import net.bioclipse.usermanager.Activator;
 import net.bioclipse.usermanager.business.IUserManager;
-import net.bioclipse.usermanager.business.UserManager;
 
 import org.apache.commons.dbcp.BasicDataSource;
-import org.apache.tools.ant.taskdefs.BUnzip2;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IConfigurationElement;
-import org.eclipse.core.runtime.IExtension;
-import org.eclipse.core.runtime.IExtensionPoint;
-import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -24,7 +16,7 @@ import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Springcontact {
 	
-	private final static String PLUGIN_ID = "net.bioclipse.brunn";
+	private final static String PLUGIN_ID = "net.bioclipse.brunn.core";
 	public static ApplicationContext CONTEXT;
 	
 	public static Object getBean(String beanName){
@@ -55,16 +47,22 @@ public class Springcontact {
 			
 			BasicDataSource dataSource = (BasicDataSource) CONTEXT.getBean("dataSource");
 			IUserManager userManager = Activator.getDefault().getUserManager();
-			dataSource.setUrl(      userManager.getProperty("LisAccountType", "URL") );
-			dataSource.setUsername( userManager.getProperty("LisAccountType", "Database user") );
-			dataSource.setPassword( userManager.getProperty("LisAccountType", "Database password") );
-			if ( !userManager.isLoggedInWithAccountType("LisAccountType") ) {
+			List<String> accounts = userManager.getAccountIdsByAccountTypeName("BrunnAccountType");
+			if (accounts.size() > 1) {
+				throw new IllegalStateException("Found multiple Brunn accounts. Brunn doesn't support that in this version");
+			}
+			dataSource.setUrl(      userManager.getProperty(accounts.get(0), "URL") );
+			dataSource.setUsername( userManager.getProperty(accounts.get(0), "Database user") );
+			dataSource.setPassword( userManager.getProperty(accounts.get(0), "Database password") );
+			if ( !userManager.isLoggedInWithAccountType("BrunnAccountType") ) {
 				throw new IllegalStateException("not logged in or the logged in user lacks LisAccount");
 			}
 		}
 		//not running from the lis_gui project but rather running test code
 		//TODO: this is ugly with a special case for running tests figure out something else maybee?
 		catch(NullPointerException e) {
+			System.out.println("Running in tests mode. If not tunning tests this is wrong.");
+			System.out.println("See Springcontact.rebuildContext()");
 			CONTEXT = new ClassPathXmlApplicationContext("applicationContext.xml");
 		}
     }
