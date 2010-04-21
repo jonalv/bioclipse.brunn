27b2063c4e738dc463e2886214a79d965cddd74d
jonalv <jonathan.alvarsson@gmail.com>
2010-04-21 13:23:03 +0200

    fix for borken file headers by masak++

--- plugins/de.kupzog.KTable/src/de/kupzog/ktable/editors/KTableCellEditorComboText.java
+++ plugins/de.kupzog.KTable/src/de/kupzog/ktable/editors/KTableCellEditorComboText.java
@@ -1,5 +1,5 @@
 
-/*******************************************************************************
+/* *****************************************************************************
  * Copyright (C) 2004 by Friederich Kupzog Elektronik & Software
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
